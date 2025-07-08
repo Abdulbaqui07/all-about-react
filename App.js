2bfa31adc7315c6ae7740a8e7586d90b41864b12
Abdulbaqui <abdulbaquia@neelafilm.com>
2025-07-08 15:12:47 +0530

    chap-02

--- App.js
+++ App.js
@@ -19,11 +19,14 @@
 //         // React.createElement("h1", {}, "I am an H1 tag!")));
 //         [React.createElement("h1", {}, "I am an H1 tag!"), React.createElement("h2", {}, "I am an H2 tag!")]
 //     ));
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" },
     [
         React.createElement("div", { id: "child" },
             // React.createElement("h1", {}, "I am an H1 tag!")));
-            [React.createElement("h1", {}, "I am an H1 tag!"), React.createElement("h2", {}, "I am an H2 tag!")]
+            [React.createElement("h1", {}, "This is React Course"), React.createElement("h2", {}, "Learn React from Scratch")]
         ), React.createElement("div", { id: "child2" },
             // React.createElement("h1", {}, "I am an H1 tag!")));
             [React.createElement("h1", {}, "I am an H1 tag!"), React.createElement("h2", {}, "I am an H2 tag!")]
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # All About React 🚀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different build for dev and prod bundles
